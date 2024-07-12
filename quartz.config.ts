63cbdfb67b9e83203484132488a1f24160c4397b
Pinxel <jimvaneijndhoven@gmail.com>
2024-07-12 20:31:25 +0200

    Quartz sync: Jul 12, 2024, 8:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "DR Academy Notes Collection",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "Pinxel-206.github.io/DR-Notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
